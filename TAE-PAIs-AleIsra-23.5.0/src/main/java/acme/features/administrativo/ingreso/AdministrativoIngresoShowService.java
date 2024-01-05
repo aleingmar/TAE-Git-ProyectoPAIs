b791aa8e15b6f9c86a83f5e08b7957ef326726c3
isrbrepin <isrbrepin@alum.us.es>
2024-01-05 18:56:54 +0100

    Enumerados, desplegables listos.

--- TAE-PAIs-AleIsra-23.5.0/src/main/java/acme/features/administrativo/ingreso/AdministrativoIngresoShowService.java
+++ TAE-PAIs-AleIsra-23.5.0/src/main/java/acme/features/administrativo/ingreso/AdministrativoIngresoShowService.java
@@ -12,13 +12,21 @@
 
 package acme.features.administrativo.ingreso;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import acme.entities.asistencia.Ingreso;
+import acme.entities.enumerados.CentroClinico;
+import acme.entities.enumerados.MotivoIngreso;
+import acme.entities.enumerados.TipoFaseProceso;
+import acme.framework.components.jsp.SelectChoices;
 import acme.framework.components.models.Tuple;
 import acme.framework.services.AbstractService;
 import acme.roles.Administrativo;
+import acme.roles.Medico;
+import acme.roles.Paciente;
 
 @Service
 public class AdministrativoIngresoShowService extends AbstractService<Administrativo, Ingreso> {
@@ -65,11 +73,60 @@ public class AdministrativoIngresoShowService extends AbstractService<Administra
 	public void unbind(final Ingreso object) {
 		assert object != null;
 
+		SelectChoices choicesPaciente;
+		SelectChoices choicesMedico;
+		final SelectChoices choicesFaseProceso;
+		final SelectChoices choicesCentroIngreso;
+		final SelectChoices choicesMotivoIngreso;
+
+		final Collection<Paciente> pacientes;
+		final Collection<Medico> medicos;
+
+		pacientes = this.repository.findAllPacientes();
+		medicos = this.repository.findAllMedicos();
+
+		choicesPaciente = SelectChoices.from(pacientes, "dni", object.getPaciente());
+		choicesMedico = SelectChoices.from(medicos, "dni", object.getMedico());
+
+		choicesFaseProceso = SelectChoices.from(TipoFaseProceso.class, object.getFaseProceso());
+		choicesCentroIngreso = SelectChoices.from(CentroClinico.class, object.getCentroIngreso());
+		choicesMotivoIngreso = SelectChoices.from(MotivoIngreso.class, object.getMotivoIngreso());
+
 		Tuple tuple;
 
-		tuple = super.unbind(object, "fechaIngreso", "faseProceso", "motivoIngreso", "centroIngreso", "fechaValoracion", "resultadoValoracion", "motivoAlta", "fechaAlta", "paciente.userAccount.identity.email", "medico.userAccount.identity.email");
+		tuple = super.unbind(object, "fechaIngreso");
+		tuple.put("paciente", choicesPaciente.getSelected().getKey());
+		tuple.put("pacientes", choicesPaciente);
+
+		tuple.put("medico", choicesMedico.getSelected().getKey());
+		tuple.put("medicos", choicesMedico);
+
+		tuple.put("faseProceso", choicesFaseProceso.getSelected().getKey());
+		tuple.put("fasesProceso", choicesFaseProceso);
+
+		tuple.put("centroIngreso", choicesCentroIngreso.getSelected().getKey());
+		tuple.put("centrosIngreso", choicesCentroIngreso);
+
+		tuple.put("motivoIngreso", choicesMotivoIngreso.getSelected().getKey());
+		tuple.put("motivosIngreso", choicesMotivoIngreso);
 
 		super.getResponse().setData(tuple);
 	}
+
+	//	@Override
+	//	public void unbind(final Application object) {
+	//		assert object != null;
+	//
+	//		SelectChoices	choices;
+	//		Tuple			tuple;
+	//
+	//		choices = SelectChoices.from(ApplicationStatus.class, object.getStatus());
+	//
+	//		tuple = super.unbind(object, "reference", "moment", "status", "statement", "skills", "qualifications");
+	//		tuple.put("masterId", object.getJob().getId());
+	//		tuple.put("statuses", choices);
+	//
+	//		super.getResponse().setData(tuple);
+	//	}
 
 }
